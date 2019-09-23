eae7141c3e8b6b9b0f74392212930e6bec93ef3c
jelledekok <99061618@mydavinci.nl>
2019-09-23 14:48:51 +0200

    creaper
    aww man

---
+++ game.js
@@ -0,0 +1,8 @@
+alert('a wild creaper popped up!!')
+var vraag1 = prompt(creaper!?);
+if (vraag1 == aw man){
+	alert('the creaper is pleased with your knowledge')
+	alert('the creaper wants to hug you')
+	var vraag3 = prompt(A.accept B.reject);
+	if (vraag3 == B)
+}
